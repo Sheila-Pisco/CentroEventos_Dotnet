0e9e0293287dbd36511ff19f47a5bf641a8224d8
MghAriana <Arianamarilla8@gmail.com>
2025-05-03 15:25:21 -0300

    agregue toString

--- CentroEventos.Aplicacion/ActividadDeportiva.cs
+++ CentroEventos.Aplicacion/ActividadDeportiva.cs
@@ -6,14 +6,21 @@ public class ActividadDeportiva
 {
     private int _id;
     private string _nombre;
-    private int _diasDispomibles;
+    private int _diasDisponibles;
     private int _cupoMax;
 
     ActividadDeportiva(int id , string nomb , int dias, int cupo){
             _id = id;
             _nombre = nomb;
-            _diasDispomibles= dias; 
+            _diasDisponibles= dias; 
             _cupoMax = cupo;  //para las incripciones <= cupo max. 
+    }
+        public bool HayLugar(int cantRegistrada) => cantRegistrada < this._cupoMax;
+    
+    public string toString(){
+        string aux="";
+        aux+=$"Actividad Deporiva= {_id} \nNombre: {_nombre} \n Dias disponibles {_diasDisponibles} \n Cupo maximo de actividad {_cupoMax}";
+        return aux;
     }
     
 }
--- CentroEventos.Aplicacion/Usuario.cs
+++ CentroEventos.Aplicacion/Usuario.cs
@@ -7,14 +7,19 @@ public class Usuario
     private string _email;
     private string _contrasenia;
     private string _nombreUsuario;
-    private string Permisos; //crear una clase con Permisos o validaciones. 
+    private Permisos _permisos; //crear una clase con Permisos o validaciones. 
 
     Usuario(string mail, string contr, string nomb, string per){
         _email = mail;
         _contrasenia = contr;
         _nombreUsuario = nomb;
-        Permisos = per;
+        _permisos = per;
     }
     
+    public string toString(){
+        string aux="";
+        aux+=$"Usuario = {_nombreUsuario} email: {_email}";
+        return aux;
+    }
 
 }

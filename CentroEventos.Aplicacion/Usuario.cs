445229238c425a07ee1a1fc1eebf1bd282f3eaca
MghAriana <Arianamarilla8@gmail.com>
2025-05-05 15:49:22 -0300

    usuario, cache

--- CentroEventos.Aplicacion/Usuario.cs
+++ CentroEventos.Aplicacion/Usuario.cs
@@ -5,13 +5,13 @@ namespace CentroEventos.Aplicacion;
 public class Usuario
 {   
     private string _email;
-    private string _contrasenia;
+    private string _contraseña;
     private string _nombreUsuario;
     private Permisos _permisos; //crear una clase con Permisos o validaciones. 
 
     Usuario(string mail, string contr, string nomb, string per){
         _email = mail;
-        _contrasenia = contr;
+        _contraseña = contr;
         _nombreUsuario = nomb;
         _permisos = per;
     }

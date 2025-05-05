b08d0b5d5da5db370c935a25c6eae59147517831
VickyAg22 <vickag2722@gmail.com>
2025-05-04 21:48:07 -0300

    Personas,Docente,Estudiante listos con sus respectivos toString :)

--- CentroEventos.Aplicacion/Docente.cs
+++ CentroEventos.Aplicacion/Docente.cs
@@ -2,7 +2,19 @@ using System;
 
 namespace CentroEventos.Aplicacion;
 
-public class Docente
+public class Docente : Persona
 {
+    private int _matricula;
+    private int _anioIngreso;
 
+    public Docente(int matri, int anio, int id, int carnet, string nom, string ape, string direc, string facu, int tel, string uncorreo) : base(id, carnet,nom,ape,direc,facu,tel,uncorreo)
+    {
+        this._matricula = matri;
+        this._anioIngreso = anio;
+    }
+    public string toString(){
+        string aux="";
+        aux+=$" {base.toString()} \nMatricula: {_matricula} \nAño Ingreso: {_anioIngreso}";
+        return aux;
+    }
 }
--- CentroEventos.Aplicacion/Estudiante.cs
+++ CentroEventos.Aplicacion/Estudiante.cs
@@ -2,7 +2,18 @@ using System;
 
 namespace CentroEventos.Aplicacion;
 
-public class Estudiante
+public class Estudiante : Persona
 {
-
+    private int _numAlumno;
+    private string _carrera;
+    public Estudiante(int numalum, int carre, int id, int carnet, string nom, string ape, string direc, string facu, int tel, string uncorreo) : base(id, carnet,nom,ape,direc,facu,tel,uncorreo)
+    {
+        this._numAlumno = numalum;
+        this._carrera = carre;
+    }
+    public string toString(){
+        string aux="";
+        aux+=$" {base.toString()} \nNumero Alumno: {_numAlumno} /nCarrera: {_carrera}";
+        return aux;
+    }
 }
